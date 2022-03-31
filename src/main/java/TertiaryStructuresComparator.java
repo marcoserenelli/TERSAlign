74636f68df6dd1876cea41457e7b1347f03a6065
FilippoLampa <pippolampa@gmail.com>
2022-03-31 17:31:04 +0200

    Distance matrix algorithm now works for both protein and RNA based respectively on Alpha Carbons and Phosphorus

--- src/main/java/TertiaryStructuresComparator.java
+++ src/main/java/TertiaryStructuresComparator.java
@@ -8,6 +8,7 @@ public class TertiaryStructuresComparator {
         PDBFileReader pdbreader = new PDBFileReader();
         try{
             Structure struc = pdbreader.getStructure(filename);
+            System.out.println(struc.getChains(0).get(0));
             printAmminoacidDistanceMatrix(calcuateAmminoacidDistanceMatrix(struc));
         } catch (Exception e){
             e.printStackTrace();
@@ -16,28 +17,52 @@ public class TertiaryStructuresComparator {
 
     private static Double[][] calcuateAmminoacidDistanceMatrix(Structure struc){
         int totalSequenceLength = 0;
-        for(Chain currentChain : struc.getChains()) 
-           totalSequenceLength += currentChain.getSeqResLength();
-        Double[][] amminoAcidDistancesMatrix = new Double[totalSequenceLength][totalSequenceLength];
-        int amminoCount = 0;
-        for(Chain currentChain: struc.getChains()){
-            Atom amminoacidAlphaCarbon;
-            for (Group currentAmminoacid : currentChain.getAtomGroups()) {
-                int comparedAmminoCount = 0;
-                for(Chain comparisonChain : struc.getChains()) {
-                    amminoacidAlphaCarbon = currentAmminoacid.getAtom("CA");
-                    Atom comparedAmminoacidAlphaCarbon;
-                    for (Group comparisonAmminoacid : comparisonChain.getAtomGroups()) {
-                        if (currentChain.isProtein() && comparisonChain.isProtein()) {
-                            comparedAmminoacidAlphaCarbon = comparisonAmminoacid.getAtom("CA");
-                            amminoAcidDistancesMatrix[amminoCount][comparedAmminoCount] = Calc.getDistance(amminoacidAlphaCarbon, comparedAmminoacidAlphaCarbon);
-                            comparedAmminoCount++;
-                        }
-                    }
-                }
-                amminoCount++;
+        for(Chain currentChain : struc.getChains()) {
+            for(Group currentGroup : currentChain.getAtomGroups()){
+                if(!(currentGroup.getType() == GroupType.HETATM))
+                    totalSequenceLength++;
             }
         }
+        Double[][] amminoAcidDistancesMatrix = new Double[totalSequenceLength][totalSequenceLength];
+        int amminoCount = 0;
+        for(Chain currentChain: struc.getChains()) {
+                Atom amminoacidAlphaCarbon;
+                for (Group currentAmminoacid : currentChain.getAtomGroups()) {
+
+                    if(!(currentAmminoacid.getType() == GroupType.HETATM)) {
+                        //è sicuramente proteina o nucleo
+                        if (currentChain.isNucleicAcid() || currentChain.isProtein()) {
+                            if (currentChain.isProtein())
+                                amminoacidAlphaCarbon = currentAmminoacid.getAtom("CA");
+                            else {
+                                amminoacidAlphaCarbon = currentAmminoacid.getAtom("P");
+                            }
+                            int comparedAmminoCount = 0;
+                            for (Chain comparisonChain : struc.getChains()) {
+                                for (Group comparisonAmminoacid : comparisonChain.getAtomGroups()) {
+                                    if(!(comparisonAmminoacid.getType() == GroupType.HETATM)) {
+                                        Atom comparedAmminoacidAlphaCarbon;
+                                        if (currentChain.isNucleicAcid() || currentChain.isProtein()) {
+                                            if (comparisonChain.isProtein()) {
+                                                comparedAmminoacidAlphaCarbon = comparisonAmminoacid.getAtom("CA");
+                                                if (comparedAmminoacidAlphaCarbon == null)
+                                                    System.out.println(comparisonAmminoacid + " amino");
+                                            } else {
+                                                comparedAmminoacidAlphaCarbon = comparisonAmminoacid.getAtom("P");
+                                                if (comparedAmminoacidAlphaCarbon == null)
+                                                    System.out.println(comparisonAmminoacid + " nucleo");
+                                            }
+                                            amminoAcidDistancesMatrix[amminoCount][comparedAmminoCount] = Calc.getDistance(amminoacidAlphaCarbon, comparedAmminoacidAlphaCarbon);
+                                            comparedAmminoCount++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        amminoCount++;
+                    }
+                }
+            }
         return amminoAcidDistancesMatrix;
     }
 
